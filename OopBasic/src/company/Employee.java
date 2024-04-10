89d88f0423741d47b1bf24887054f2a12c365a73
dir-FY24takahashiyuuki <yuki.takahashi.b138@dirbato.co.jp>
2024-04-10 10:32:08 +0900

    最初のコミット

--- OopBasic/src/company/Employee.java
+++ OopBasic/src/company/Employee.java
@@ -1,34 +1,37 @@
 package company;
 
-public class Employee {
-	 // フィールド
-	protected final String name;
-    private final Department department;
+public abstract class Employee implements Workable {
+    // フィールド
+    protected final String name;
+    protected final Department department;
     private final String position;
     private final int employeeId;
-	 
-	// コンストラクター
+
+    // コンストラクター
     public Employee(String name, Department department, String position, int employeeId) {
-	        this.name = name;
-	        this.department = department;
-	        this.position = position;
-	        this.employeeId = employeeId;
+        this.name = name;
+        this.department = department;
+        this.position = position;
+        this.employeeId = employeeId;
+    }
+
+    // 報告メソッド
+    public void report(int times) {
+        System.out.println(times + "回目の報告をします。役職：" + position + "、名前：" + name);
+    }
+
+    // 報告メソッド（オーバーロード）
+    public void report() {
+        report(1);
+    }
+    
+    // 会議に参加するメソッド
+    public abstract void joinMeeting() ;
+   
+
+    // 働くメソッドを実装
+    @Override
+    public void work() {
+        System.out.println("正社員として働きます。名前：" + name + slogan);
 	        }
-
-	 // 報告メソッド
-	    public void report(int times) {
-	        System.out.println(times + "回目の報告をします。役職：" + position + "、名前：" + name);
-	    }
-
-	    // 報告メソッド（オーバーロード）
-	    public void report() {
-	        report(1);
-	    }
-	    
-	        // 会議に参加するメソッド
-	        public void joinMeeting() {
-	            department.meeting();
-	            System.out.println("→上記の会議に参加します。部署：" + department.getName() + "名前：" + name);
-	    }
-	    
 	}
